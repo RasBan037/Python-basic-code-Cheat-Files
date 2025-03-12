e5f615fba561a1e1fa8759cbfd9c2f2c3ce89522
RasBan037 <rasban037@yahoo.com>
2025-03-12 23:11:40 +0000

    testing connection

--- main.py
+++ main.py
@@ -89,3 +89,4 @@ days_to_unit(91, "Very good")
 print("**********ACCEPTING USER INPUT*********")
 user_input = input("Hi, Enter number of days!\n") #\n moves cursor to the next line after executing code
 print(user_input)
+print("Good")
